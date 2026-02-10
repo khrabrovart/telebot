4c2cb6e29affa137af8f90623393d7564a1d8941
Artur Khrabrov <artur.khrabrov@moonpig.com>
2026-02-11 00:42:33 +0100

    Add stream

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -12,9 +12,11 @@ resource "aws_dynamodb_table" "posting_rules" {
 }
 
 resource "aws_dynamodb_table" "bots" {
-  name         = "${local.app_name}-bots"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "Id"
+  name             = "${local.app_name}-bots"
+  billing_mode     = "PAY_PER_REQUEST"
+  hash_key         = "Id"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = "Id"
