5487bcc7589e76a72a2e588727d474b34b1a2ef4
Artur Khrabrov <artur.khrabrov@moonpig.com>
2026-09-11 15:16:40 +0200

    Remove assume role

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,9 +2,3 @@ variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
 }
-
-variable "aws_assume_role_arn" {
-  description = "IAM role ARN to assume when managing resources in the member account"
-  type        = string
-  sensitive   = true
-}
