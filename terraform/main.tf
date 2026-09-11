260e2ee6feb03e3a2c0bdcde1c9b524e5472d74d
Artur Khrabrov <artur.khrabrov@moonpig.com>
2026-09-11 15:25:24 +0200

    Allow deploying in the new account

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,10 @@ terraform {
 provider "aws" {
   region = var.aws_region
 
+  assume_role {
+    role_arn = var.aws_assume_role_arn
+  }
+
   default_tags {
     tags = {
       Project   = "Telebot"
